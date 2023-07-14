cce2e89bc32e04456059774e36723f6eab5419e3
2002yyf <1207835084@qq.com>
2023-07-14 10:01:27 +0800

    学生充值记录新增、查询

--- src/main/resources/school.sql
+++ src/main/resources/school.sql
@@ -11,11 +11,31 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 14/07/2023 08:26:06
+ Date: 14/07/2023 10:01:14
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for business
+-- ----------------------------
+DROP TABLE IF EXISTS `business`;
+CREATE TABLE `business`  (
+  `bid` int(0) NOT NULL COMMENT 'id',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类型',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`bid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of business
+-- ----------------------------
+INSERT INTO `business` VALUES (3, '123', NULL, NULL, NULL);
+INSERT INTO `business` VALUES (4, '123', NULL, NULL, NULL);
+INSERT INTO `business` VALUES (6, '123', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for dorm
@@ -34,6 +54,37 @@ CREATE TABLE `dorm`  (
 -- Records of dorm
 -- ----------------------------
 INSERT INTO `dorm` VALUES (1, 1, 101, 173.8, 49.5);
+
+-- ----------------------------
+-- Table structure for person_recharge
+-- ----------------------------
+DROP TABLE IF EXISTS `person_recharge`;
+CREATE TABLE `person_recharge`  (
+  `rid` int(0) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `sid` int(0) NOT NULL COMMENT '学生id',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类型',
+  `amount` double(10, 1) NULL DEFAULT NULL COMMENT '充值金额',
+  `time` datetime(0) NULL DEFAULT NULL COMMENT '充值时间',
+  PRIMARY KEY (`rid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of person_recharge
+-- ----------------------------
+INSERT INTO `person_recharge` VALUES (1, 1, '热水', 50.0, '2023-07-13 09:01:13');
+INSERT INTO `person_recharge` VALUES (2, 1, '一卡通', 30.0, NULL);
+INSERT INTO `person_recharge` VALUES (3, 2, '热水', 50.0, NULL);
+INSERT INTO `person_recharge` VALUES (4, 1, '热水', 4.3, NULL);
+INSERT INTO `person_recharge` VALUES (5, 1, '热水', 50.0, '2023-07-14 09:59:27');
+INSERT INTO `person_recharge` VALUES (6, 1, '热水', 50.0, '2023-07-14 09:59:31');
+INSERT INTO `person_recharge` VALUES (7, 1, '热水', 50.0, '2023-07-14 09:59:33');
+INSERT INTO `person_recharge` VALUES (8, 1, '热水', 50.0, '2023-07-14 09:59:33');
+INSERT INTO `person_recharge` VALUES (9, 1, '热水', 50.0, '2023-07-14 09:59:34');
+INSERT INTO `person_recharge` VALUES (10, 1, '热水', 50.0, '2023-07-14 09:59:34');
+INSERT INTO `person_recharge` VALUES (11, 1, '热水', 50.0, '2023-07-14 09:59:35');
+INSERT INTO `person_recharge` VALUES (12, 1, '热水', 50.0, '2023-07-14 09:59:35');
+INSERT INTO `person_recharge` VALUES (13, 1, '热水', 50.0, '2023-07-14 09:59:35');
+INSERT INTO `person_recharge` VALUES (14, 1, '热水', 50.0, '2023-07-14 09:59:38');
 
 -- ----------------------------
 -- Table structure for student
@@ -57,6 +108,8 @@ CREATE TABLE `student`  (
 -- ----------------------------
 INSERT INTO `student` VALUES (1, NULL, '123', NULL, NULL, NULL, 363.8, 1909.0, NULL);
 INSERT INTO `student` VALUES (2, NULL, '456', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `student` VALUES (6, NULL, '123', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `student` VALUES (7, NULL, '123', NULL, NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `student` VALUES (17, NULL, '12', NULL, NULL, '101', NULL, NULL, NULL);
 INSERT INTO `student` VALUES (100, NULL, '12', NULL, NULL, NULL, NULL, NULL, NULL);
 
