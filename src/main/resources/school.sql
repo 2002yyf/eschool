ff105e6b83a6c40f8f743d1488e066d7cfee4882
2002yyf <1207835084@qq.com>
2023-07-15 11:55:05 +0800

    数据库文件更新

--- src/main/resources/school.sql
+++ src/main/resources/school.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 15/07/2023 11:12:52
+ Date: 15/07/2023 11:54:51
 */
 
 SET NAMES utf8mb4;
@@ -22,7 +22,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `business`;
 CREATE TABLE `business`  (
-  `bid` int(0) NOT NULL COMMENT 'id',
+  `bid` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类型',
@@ -33,16 +33,20 @@ CREATE TABLE `business`  (
 -- ----------------------------
 -- Records of business
 -- ----------------------------
-INSERT INTO `business` VALUES (3, '123', NULL, NULL, NULL);
-INSERT INTO `business` VALUES (4, '123', NULL, NULL, NULL);
-INSERT INTO `business` VALUES (6, '123', NULL, NULL, NULL);
+INSERT INTO `business` VALUES (7, '123', '商户1', '奶茶', '商铺101');
+INSERT INTO `business` VALUES (8, '123', '商户2', '水果', '商铺102');
+INSERT INTO `business` VALUES (9, '123', '商户3', '糕点', '商铺103');
+INSERT INTO `business` VALUES (10, '123', '商户4', '糕点', '商铺104');
+INSERT INTO `business` VALUES (11, '123', '商户5', '糕点', '商铺105');
+INSERT INTO `business` VALUES (12, '123', '商户6', '糕点', '商铺106');
+INSERT INTO `business` VALUES (13, '123', '商户7', '奶茶', '商铺107');
 
 -- ----------------------------
 -- Table structure for dorm
 -- ----------------------------
 DROP TABLE IF EXISTS `dorm`;
 CREATE TABLE `dorm`  (
-  `dormid` int(0) NOT NULL COMMENT '主键',
+  `dormid` int(0) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `building` int(0) NULL DEFAULT NULL COMMENT '楼号',
   `room` int(0) NULL DEFAULT NULL COMMENT '宿舍号',
   `water` double(255, 1) NULL DEFAULT NULL COMMENT '水费',
@@ -53,7 +57,36 @@ CREATE TABLE `dorm`  (
 -- ----------------------------
 -- Records of dorm
 -- ----------------------------
-INSERT INTO `dorm` VALUES (1, 1, 101, 173.8, 49.5);
+INSERT INTO `dorm` VALUES (2, 1, 101, 234.3, 262.2);
+INSERT INTO `dorm` VALUES (3, 1, 102, 227.2, 270.3);
+INSERT INTO `dorm` VALUES (4, 1, 103, 229.1, 212.7);
+INSERT INTO `dorm` VALUES (5, 1, 104, 237.4, 211.0);
+INSERT INTO `dorm` VALUES (6, 1, 105, 246.2, 220.2);
+INSERT INTO `dorm` VALUES (7, 1, 106, 234.5, 220.9);
+INSERT INTO `dorm` VALUES (8, 1, 107, 247.0, 281.7);
+INSERT INTO `dorm` VALUES (9, 1, 108, 229.1, 273.4);
+INSERT INTO `dorm` VALUES (10, 1, 109, 257.5, 222.7);
+INSERT INTO `dorm` VALUES (11, 1, 110, 248.0, 249.7);
+INSERT INTO `dorm` VALUES (12, 2, 101, 219.9, 286.5);
+INSERT INTO `dorm` VALUES (13, 2, 102, 278.9, 297.1);
+INSERT INTO `dorm` VALUES (14, 2, 103, 243.6, 295.0);
+INSERT INTO `dorm` VALUES (15, 2, 104, 272.3, 212.4);
+INSERT INTO `dorm` VALUES (16, 2, 105, 266.8, 221.8);
+INSERT INTO `dorm` VALUES (17, 2, 106, 288.4, 287.5);
+INSERT INTO `dorm` VALUES (18, 2, 107, 242.5, 250.4);
+INSERT INTO `dorm` VALUES (19, 2, 108, 232.0, 279.0);
+INSERT INTO `dorm` VALUES (20, 2, 109, 247.3, 203.3);
+INSERT INTO `dorm` VALUES (21, 2, 110, 282.1, 201.5);
+INSERT INTO `dorm` VALUES (22, 3, 101, 228.0, 269.8);
+INSERT INTO `dorm` VALUES (23, 3, 102, 211.4, 216.4);
+INSERT INTO `dorm` VALUES (24, 3, 103, 209.3, 288.7);
+INSERT INTO `dorm` VALUES (25, 3, 104, 235.4, 237.0);
+INSERT INTO `dorm` VALUES (26, 3, 105, 239.2, 230.3);
+INSERT INTO `dorm` VALUES (27, 3, 106, 202.7, 289.1);
+INSERT INTO `dorm` VALUES (28, 3, 107, 241.3, 245.2);
+INSERT INTO `dorm` VALUES (29, 3, 108, 298.2, 224.5);
+INSERT INTO `dorm` VALUES (30, 3, 109, 291.3, 274.2);
+INSERT INTO `dorm` VALUES (31, 3, 110, 266.7, 281.8);
 
 -- ----------------------------
 -- Table structure for dorm_maintenance
@@ -176,7 +209,7 @@ CREATE TABLE `order_water_record`  (
   `number` int(0) NULL DEFAULT NULL COMMENT '购买数量',
   `time` datetime(0) NULL DEFAULT NULL COMMENT '购买时间',
   PRIMARY KEY (`rid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 33 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order_water_record
@@ -280,11 +313,36 @@ CREATE TABLE `person_recharge`  (
 -- ----------------------------
 -- Records of person_recharge
 -- ----------------------------
-INSERT INTO `person_recharge` VALUES (1, 1, '热水', 50.0, '2023-07-13 09:01:13');
-INSERT INTO `person_recharge` VALUES (2, 1, '一卡通', 30.0, NULL);
-INSERT INTO `person_recharge` VALUES (3, 2, '热水', 50.0, NULL);
-INSERT INTO `person_recharge` VALUES (4, 1, '热水', 4.3, NULL);
-INSERT INTO `person_recharge` VALUES (5, 1, '热水', 50.0, '2023-07-14 09:59:27');
+INSERT INTO `person_recharge` VALUES (15, 19, '热水', 53.4, '2022-06-21 08:02:11');
+INSERT INTO `person_recharge` VALUES (16, 18, '一卡通', 63.3, '2023-01-16 22:14:43');
+INSERT INTO `person_recharge` VALUES (17, 8, '一卡通', 67.7, '2022-01-24 00:25:35');
+INSERT INTO `person_recharge` VALUES (18, 15, '一卡通', 65.0, '2023-01-19 20:01:28');
+INSERT INTO `person_recharge` VALUES (19, 20, '一卡通', 52.2, '2022-11-05 05:52:38');
+INSERT INTO `person_recharge` VALUES (20, 9, '热水', 65.0, '2022-07-06 11:24:49');
+INSERT INTO `person_recharge` VALUES (21, 16, '热水', 56.1, '2023-04-13 06:01:02');
+INSERT INTO `person_recharge` VALUES (22, 13, '一卡通', 56.1, '2022-07-26 11:43:18');
+INSERT INTO `person_recharge` VALUES (23, 14, '一卡通', 55.1, '2022-11-21 18:46:55');
+INSERT INTO `person_recharge` VALUES (24, 18, '热水', 56.5, '2023-07-17 07:57:10');
+INSERT INTO `person_recharge` VALUES (25, 13, '热水', 57.2, '2022-01-20 13:22:37');
+INSERT INTO `person_recharge` VALUES (26, 14, '热水', 60.6, '2022-05-21 21:22:52');
+INSERT INTO `person_recharge` VALUES (27, 12, '热水', 66.0, '2023-02-04 00:43:41');
+INSERT INTO `person_recharge` VALUES (28, 10, '热水', 55.0, '2023-06-02 01:16:21');
+INSERT INTO `person_recharge` VALUES (29, 3, '热水', 54.0, '2022-07-08 17:05:18');
+INSERT INTO `person_recharge` VALUES (30, 10, '热水', 54.6, '2023-06-29 06:56:50');
+INSERT INTO `person_recharge` VALUES (31, 11, '热水', 56.6, '2022-10-18 15:11:41');
+INSERT INTO `person_recharge` VALUES (32, 15, '一卡通', 51.2, '2022-02-05 14:31:16');
+INSERT INTO `person_recharge` VALUES (33, 9, '热水', 65.6, '2022-04-01 23:21:00');
+INSERT INTO `person_recharge` VALUES (34, 6, '热水', 67.4, '2022-04-08 08:38:05');
+INSERT INTO `person_recharge` VALUES (35, 19, '一卡通', 55.4, '2022-08-01 05:48:12');
+INSERT INTO `person_recharge` VALUES (36, 10, '一卡通', 56.0, '2022-05-02 16:39:11');
+INSERT INTO `person_recharge` VALUES (37, 20, '一卡通', 68.0, '2023-07-20 20:33:11');
+INSERT INTO `person_recharge` VALUES (38, 8, '一卡通', 57.7, '2022-09-04 15:01:50');
+INSERT INTO `person_recharge` VALUES (39, 11, '一卡通', 54.2, '2023-05-19 06:53:56');
+INSERT INTO `person_recharge` VALUES (40, 11, '一卡通', 52.4, '2022-07-28 09:06:02');
+INSERT INTO `person_recharge` VALUES (41, 14, '一卡通', 57.5, '2022-10-19 06:25:58');
+INSERT INTO `person_recharge` VALUES (42, 5, '一卡通', 50.8, '2023-07-13 23:04:35');
+INSERT INTO `person_recharge` VALUES (43, 11, '一卡通', 51.0, '2023-05-12 02:33:30');
+INSERT INTO `person_recharge` VALUES (44, 2, '热水', 55.4, '2022-10-24 23:57:12');
 
 -- ----------------------------
 -- Table structure for student
@@ -309,5 +367,18 @@ CREATE TABLE `student`  (
 INSERT INTO `student` VALUES (1, '张三', '123', '2020', '4', '503', 363.8, 1909.0, '10086');
 INSERT INTO `student` VALUES (2, '李四', '456', '2021', '5', '602', 345.3, 20.3, '10087');
 INSERT INTO `student` VALUES (6, '王五', '123', '2022', '6', '102', 65.0, 35.0, '10088');
+INSERT INTO `student` VALUES (20052317, '学生2', 'password', '2020', '5', '103', 197.1, 28.8, '15249291795');
+INSERT INTO `student` VALUES (20052390, '学生12', 'password', '2020', '3', '101', 234.0, 11.6, '15867354230');
+INSERT INTO `student` VALUES (20052430, '学生6', 'password', '2023', '1', '203', 205.3, 14.3, '15464746837');
+INSERT INTO `student` VALUES (20052461, '学生9', 'password', '2020', '1', '505', 288.3, 24.8, '15671513606');
+INSERT INTO `student` VALUES (20052475, '学生3', 'password', '2021', '5', '102', 105.7, 13.5, '15642477570');
+INSERT INTO `student` VALUES (20052481, '学生4', 'password', '2020', '6', '703', 217.3, 23.9, '15898019342');
+INSERT INTO `student` VALUES (20052493, '学生5', 'password', '2022', '1', '406', 145.9, 20.9, '15858369812');
+INSERT INTO `student` VALUES (20052500, '学生8', 'password', '2021', '5', '703', 265.9, 13.7, '16040200620');
+INSERT INTO `student` VALUES (20052536, '学生7', 'password', '2021', '6', '203', 242.6, 11.2, '15325810229');
+INSERT INTO `student` VALUES (20052575, '学生10', 'password', '2022', '3', '103', 168.0, 16.3, '15941683708');
+INSERT INTO `student` VALUES (20052602, '学生13', 'password', '2022', '6', '703', 242.1, 15.3, '15950423654');
+INSERT INTO `student` VALUES (20052611, '学生11', 'password', '2023', '4', '203', 222.2, 19.6, '15940325423');
+INSERT INTO `student` VALUES (20052704, '学生1', 'password', '2022', '7', '103', 262.1, 16.5, '15197392983');
 
 SET FOREIGN_KEY_CHECKS = 1;
